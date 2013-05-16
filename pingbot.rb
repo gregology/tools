126bf6b5a1f3efb64f3c16c239fd2ae29d9259b5
Gregory Clarke <greg@gho.st>
2013-05-16 12:00:27 -0300

    Update pingbot.rb

--- pingbot.rb
+++ pingbot.rb
@@ -1,14 +1,26 @@
 #!/usr/bin/env ruby
+require 'socket'
 
-networkprefix = '192.168.0'
+def local_ip
+  orig, Socket.do_not_reverse_lookup = Socket.do_not_reverse_lookup, true  # turn off reverse DNS resolution temporarily
+
+  UDPSocket.open do |s|
+    s.connect '64.233.187.99', 1
+    s.addr.last
+  end
+ensure
+  Socket.do_not_reverse_lookup = orig
+end
 
 def pingable?(addr)
   output = `ping -c 1 #{addr}`
   !output.include? "100% packet loss"
 end
 
+networkprefix = local_ip.gsub( (/\.\d$|\.\d\d$|\.\d\d\d$/), '' ).to_s
+
 host = 1
 255.times do
   puts "#{networkprefix}.#{host} " + (pingable?("#{networkprefix}.#{host}") ? "up" : "down")
-  oct += 1
+  host += 1
 end
